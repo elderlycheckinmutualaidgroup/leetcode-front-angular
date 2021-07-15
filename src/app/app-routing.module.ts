0203695e736ca9c1dd74e7e57148c906fc801646
longlonglu <48895042+longlonglu@users.noreply.github.com>
2021-07-15 10:30:38 -0400

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MyViewComponent } from './my-view/my-view.component';
+import { MyComponent } from './my/my.component';
+import { QuestionViewComponent } from './question-view/question-view.component';
+import { QuestionComponent } from './question/question.component';
+import { RankingComponent } from './ranking/ranking.component';
+import { AuthServiceService } from './services/auth-service/auth-service.service';
+import { UploadQuestionComponent } from './upload-question/upload-question.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'question', component: QuestionComponent },
+  { path: 'question-view/:questionId', component: QuestionViewComponent },
+  {
+    path: 'uploadQuestion',
+    component: UploadQuestionComponent,
+    canActivate: [AuthServiceService],
+  },
+  { path: 'ranking', component: RankingComponent },
+  { path: 'topic', component: QuestionComponent },
+  { path: 'my', component: MyComponent, canActivate: [AuthServiceService] },
+  {
+    path: 'my-view/:questionId',
+    component: MyViewComponent,
+    canActivate: [AuthServiceService],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,72 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
+import { MatChipsModule } from '@angular/material/chips';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { UserLayoutComponent } from './layout/user-layout/user-layout.component';
+import { GuestLayoutComponent } from './layout/guest-layout/guest-layout.component';
+import { QuestionComponent } from './question/question.component';
+import { QuestionViewComponent } from './question-view/question-view.component';
+import { UploadQuestionComponent } from './upload-question/upload-question.component';
+import { RankingComponent } from './ranking/ranking.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MyComponent } from './my/my.component';
+import { MyViewComponent } from './my-view/my-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserLayoutComponent,
+    GuestLayoutComponent,
+    QuestionComponent,
+    QuestionViewComponent,
+    UploadQuestionComponent,
+    RankingComponent,
+    MyComponent,
+    MyViewComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatInputModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    HttpClientModule,
+    MatChipsModule,
+    DragDropModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
